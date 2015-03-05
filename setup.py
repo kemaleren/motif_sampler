c60e445c9ba571bad6198b525a9c917df3d7aa12
Kemal Eren <kemal@kemaleren.com>
2015-03-05 02:09:20 -0800

    changed name in setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ from Cython.Build import cythonize
 import numpy
 
 setup(
-    name = "find motifs",
+    name = "motif sampler",
     ext_modules = cythonize("*.pyx", include_path = [numpy.get_include()]),
 )
