f0ea5a6ce4c62ae14c60f96e9829d3addfbf715b
Kemal Eren <kemal@kemaleren.com>
2015-03-05 17:30:59 -0800

    added compiler flags

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
-from distutils.core import setup
+from distutils.core import setup, Extension
 from Cython.Build import cythonize
 
 import numpy
 
+extensions = [
+    Extension("_motif_sampler", ["_motif_sampler.pyx"],
+              include_dirs = [numpy.get_include()],
+              extra_compile_args=['-O3', '-march=native', '-ffast-math', '-funroll-loops'],
+          )
+]
+
 setup(
     name = "motif sampler",
-    ext_modules = cythonize("*.pyx", include_path = [numpy.get_include()]),
+    ext_modules = cythonize(extensions),
 )
